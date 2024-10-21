a729c94014394d8705a3ee3cb7afb418b6048f62
theinhan8 <159587720+theinhan8@users.noreply.github.com>
2024-10-21 15:13:56 -0500

    created the js for index.html

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 function init(){
-//add your javascrip between these two lines of code
+  var button = document.getElementById('entrybutton');
+  
+  function showMeText(){
+    var textbook = document.getElementById('entryinput');
+    document.getElementById('textoutput').innerHTML = textbook.value;
+    alert("Thein Han: " + textbook.value);
+  }
  
-
-
-
-
-
-
+  button.addEventListener('click', showMeText);
+}
 
 window.addEventListener('load', init);
