d652313cca13b872af8eb77ee3a1f903c30d9f7b
theinhan8 <159587720+theinhan8@users.noreply.github.com>
2024-10-21 15:20:52 -0500

    updated script.js

--- final_script.js
+++ final_script.js
@@ -1,10 +1,10 @@
 function init(){
   var button = document.getElementById('entrybutton');
-  
+
   function showMeText(){
     var textbox = document.getElementById('entryinput');
     document.getElementById('textoutput').innerHTML = textbox.value;
-    alert("Thein Han: " + textbox.value);
+    alert("Daniel Krieglstein: " + textbox.value);
   }
 
   button.addEventListener('click', showMeText);
